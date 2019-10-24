c03bab6d5303b4e98786c03df54110e4fdf9030f
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-10-24 20:51:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,8 @@ sql = <<-SQL
   end
 
   def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
+    sql = <<-SQL
+    "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
 end
